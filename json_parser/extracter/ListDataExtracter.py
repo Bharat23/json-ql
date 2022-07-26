aebc4677626f93baa42a29017e9fab07c231a7dd
Bharat Sinha <bharat.sinha.2307@gmail.com>
2022-07-25 22:37:55 -0600

    add base code from wpt-parser

---
+++ json_parser/extracter/ListDataExtracter.py
@@ -0,0 +1,12 @@
+from .DataExtracter import DataExtracter
+
+
+class ListDataExtracter(DataExtracter):
+    def __init__(self):
+        super().__init__()
+
+    def extract(self, list: list, index: int):
+        try:
+            return list[index]
+        except KeyError:
+            return None
